02e1601884ab36d8564ff64b9f9d559610bb0166
ali <alboyzskeaters@gmail.com>
2018-12-09 23:57:30 +0700

    class component dalam 1 file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 
+class Header extends Component{
+	render() {
+	
+		return (
 
+			<div>
+				<h2>Makanan Khas Indonesia</h2>
+			</div>				
+		)
+
+	}
+
+}
 class App extends Component {
 	render() {
 				
 			return (
 		<div>
-		
+		//memasukan class Header 
+		<Header/>	
 		<h1>Rendering Element</h1>
 	  </div>
 	);
