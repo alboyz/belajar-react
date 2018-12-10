ed36df0b7f117dd0b44b15147e1aa7be1766ca45
ali <alboyzskeaters@gmail.com>
2018-12-10 09:38:55 +0700

    class component file terpisah

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
+//import component file terpisah
+import Header from './Header';
 
-class Header extends Component{
-	render() {
-	
-		return (
 
-			<div>
-				<h2>Makanan Khas Indonesia</h2>
-			</div>				
-		)
-
-	}
-
-}
 class App extends Component {
 	render() {
 				
 			return (
+		//	memasukan class Header 			
+							
 		<div>
-		//memasukan class Header 
 		<Header/>	
 		<h1>Rendering Element</h1>
 	  </div>
