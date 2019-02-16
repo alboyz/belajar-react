8d5f4b02b755379121c76af2a4c37c6576a32e6f
ali <alboyzskeaters@gmail.com>
2019-02-16 10:12:34 +0700

    conditional rendering 1 salah

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,8 @@ class Header extends Component{
 				super(props);
 				this.state = {
 						daftar: "Daftar Nama Masakan Nusantara",
-						dataList: this.props.List
+						dataList: this.props.List,
+						statusRendering: true
 				}
 				this.HandlePesan = this.HandlePesan.bind(this )
 		}
@@ -19,18 +20,32 @@ class Header extends Component{
 		componentDidMount(){
 				console.log("Jalan Component Did Mount")
 		}
-		render() {
-				console.log("Jalan :Render")
-				return (
-						<div>
-								<h2>Makanan Khas Indonesia</h2>
-								<p>{this.state.daftar}</p>
-								<p>{this.state.dataList}</p>
-								<a href="/"onClick={(e) => this.HandlePesan("Pesan dari Header", e)}>Halaman Header</a>
-						</div>				
-				);			
-		}
+		//				render() {
+		//				console.log("Jalan :Render")
+		//				return (
+		//						<div>
+		//								<h2>Makanan Khas Indonesia</h2>
+		//								<p>{this.state.daftar}</p>
+		//								<p>{this.state.dataList}</p>
+		//								<a href="/"onClick={(e) => this.HandlePesan("Pesan dari Header", e)}>Halaman Header</a>
+		//						</div>
 
-}
+		render(){
+				<div>
+						{this.state.statusRendering === true ? (
+								<h1>Selamat Datang</h1>
+						):(
+								<h1>Selamat Tinggal</h1>
+						)
+
+				</div>
+
+		}
+} 	
+
+
+
+
+
 
 export default Header;
