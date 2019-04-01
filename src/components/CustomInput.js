4a950dba778102f28ae77ce87e1c274d74442bbd
ali <alboyzskeaters@gmail.com>
2019-04-01 16:21:59 +0700

    refs lanjutan

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -7,18 +7,25 @@ class CustomInput extends Component{
 						value: "input pertama"	
 				}
 				this.handleInput=this.handleInput.bind(this);
+				this.handleChange=this.handleChange.bind(this);
 				this.textInput=React.createRef();
 		}
 		handleInput(){
 				this.setState({value: ""})
 				this.textInput.current.focus();
-
+		}
+		handleChange(e){
+				this.setState({value: e.target.value})
 		}
 
 		render() {
 				return (
 						<div>
-								<input type="text" value={this.state.value} ref={this.textInput} />
+								<input type="text" 
+										value={this.state.value} 
+										ref={this.textInput}
+										onChange={this.handleChange}
+								/>
 
 								<button onClick={this.handleInput}>kirim</button>
 						</div>
